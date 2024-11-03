c496511a05c01aa4b5a1aa7e248d282d292b538e
thanhnhax <thanhnha.k13@gmail.com>
2024-11-03 14:50:30 +0700

    upload file nestjs

---
+++ node44/src/email/email.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import * as nodemailer from 'nodemailer';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class MailService {
+  private transporter: nodemailer.Transporter;
+  private configService: ConfigService;
+  constructor() {
+    this.transporter = nodemailer.createTransport({
+      host: 'gmail',
+      port: 587,
+      auth: {
+        user: this.configService.get<string>('EMAIL_USER'),
+        pass: this.configService.get<string>('EMAIL_PASS'),
+      },
+    });
+  }
+}
